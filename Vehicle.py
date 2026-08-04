39128ac2741c293f77880080a79f6694bf5cf893
Dhruv Malhotra <malhotradhruv112004@gmail.com>
2026-08-04 14:46:37 +0530

    update:[Dhruv]-UC2 task done

--- Vehicle.py
+++ Vehicle.py
@@ -2,5 +2,36 @@ class Vehicles:
     def __init__(self,vehicle_id,model,battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
-        self.battery_percentage = battery_percentage
+        #self.__battery_percentage = battery_percentage
+        self.set_battery_percentage(battery_percentage)
+        self.__maintenance_status = 0# where to initialize the maintenance status??
+        self.__rental_price = 0# where to initialize the rental price??
         
+
+    #Getter battery
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+
+    #Setter battery
+    def set_battery_percentage(self,battery_percentage):
+        if battery_percentage < 0 or battery_percentage > 100:
+            print("battery_percentage cannot be more than 100 or less than 0")
+        else:
+            self.__battery_percentage = battery_percentage
+
+    #getter for maintenance 
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+    #setter for maintenance
+    def set_maintenance_status(self,status):
+        self.__maintenance_status = status
+
+    #getter for Rental_Price
+    def get_rental_price(self):  # function to get the rental price
+        return self.__rental_price
+
+    def set_rental_price(self,price):
+        if price < 0:
+            print("Rental price cannot be negative")
+        else:
+            self.__rental_price = price
--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from Vehicle import Vehicles
 
 def main():
     print("Urban Mobility & Fleet Management System")
+    print()
+    print("-")
 
     vehicle1 = Vehicles("V001", "Maruti 800", 85)
-    print(f"Vehicle ID: {vehicle1.vehicle_id}, Model: {vehicle1.model}, Battery Percentage: {vehicle1.battery_percentage}%")
+    print(f"Vehicle ID: {vehicle1.vehicle_id}, Model: {vehicle1.model}, Battery Percentage: {vehicle1.get_battery_percentage}%")
+    print(f"vehicle1.get_battery_percentage(): {vehicle1.get_battery_percentage()}% \n vehicle1.get_maintenance_status(): {vehicle1.get_maintenance_status()} \n vehicle1.get_rental_price(): {vehicle1.get_rental_price()}")
 
-# if __name__ == "__main__":
+    vehicle1.set_battery_percentage(90)
+    vehicle1.set_maintenance_status(1)
+    vehicle1.set_rental_price(10000)
+
+    print(f"After updating vehicle1's attributes:")
+    print(f"Vehicle ID: {vehicle1.vehicle_id}\n Model: {vehicle1.model} \nBattery Percentage: {vehicle1.get_battery_percentage()}%  \nMaintenance Status: {vehicle1.get_maintenance_status()} \nRental Price: {vehicle1.get_rental_price()}")
+
+
 main()
